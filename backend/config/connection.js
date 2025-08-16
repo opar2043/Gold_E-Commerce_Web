5243ccf4bc84d862724701cf98a44b8509c93c8e
maisha27 <mnanjeeba@gmail.com>
2025-08-16 20:19:33 +0600

    setup backend

---
+++ backend/config/connection.js
@@ -0,0 +1,9 @@
+// backend/config/connection.js
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+export default supabase;
